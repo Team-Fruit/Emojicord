79a36bb43689510d253c0ffeff0382a9f9cb8768
Kamesuta <kamesuta@gmail.com>
2019-06-18 03:02:18 +0900

    name

--- emojis/EmojicordConvert.py
+++ emojis/EmojicordConvert.py
@@ -12,15 +12,19 @@ if response.status_code == 200:
 # with codecs.open('discordEmojiMap.json', 'r', 'utf-8-sig') as f:
 #     jsn = json.load(f)
 
+output = {
+    'location': 'https://twemoji.maxcdn.com/2/72x72/',
+    'version': jsn['version'],
+    'versionTimestamp': jsn['versionTimestamp'],
+    'emojis': [],
+}
 for i, data in enumerate(jsn['emojiDefinitions']):
     filename = '-'.join([format(codepoint, 'x') for codepoint in data['utf32codepoints']])
-    jsn['emojiDefinitions'][i] = {
+    output['emojis'].append({
         'name': data['primaryName'],
         'strings': data['names'],
         'location': f'{filename}.png',
-    }
-
-jsn['location'] = 'https://twemoji.maxcdn.com/2/72x72/'
+    })
 
 with codecs.open('emojis.yml', 'w', 'utf-8') as f:
-    yaml.dump(jsn, f, encoding='utf8', allow_unicode=True)
+    yaml.dump(output, f, encoding='utf8', allow_unicode=True)
