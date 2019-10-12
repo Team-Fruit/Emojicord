60859b16b8ff258b5de9479b023d8c03e04636c0
Kamesuta <kamesuta@gmail.com>
2019-10-12 18:12:55 +0800

    Web Server

--- sources/universal/src/main/java/net/teamfruit/emojicord/CallbackServerInstance.java
+++ sources/universal/src/main/java/net/teamfruit/emojicord/CallbackServerInstance.java
@@ -6,6 +6,7 @@ import java.io.InterruptedIOException;
 import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.charset.StandardCharsets;
 import java.util.Locale;
 
 import javax.net.ssl.SSLServerSocketFactory;
@@ -13,6 +14,7 @@ import javax.net.ssl.SSLServerSocketFactory;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.http.ConnectionClosedException;
 import org.apache.http.HttpConnectionFactory;
+import org.apache.http.HttpEntity;
 import org.apache.http.HttpEntityEnclosingRequest;
 import org.apache.http.HttpException;
 import org.apache.http.HttpHeaders;
@@ -38,8 +40,6 @@ import org.apache.http.protocol.ResponseServer;
 import org.apache.http.protocol.UriHttpRequestHandlerMapper;
 import org.apache.http.util.EntityUtils;
 
-import com.google.common.base.Charsets;
-
 import net.teamfruit.emojicord.util.DataUtils;
 
 public class CallbackServerInstance {
@@ -52,7 +52,7 @@ public class CallbackServerInstance {
 		// Set up the HTTP protocol processor
 		final HttpProcessor httpproc = HttpProcessorBuilder.create()
 				.add(new ResponseDate())
-				.add(new ResponseServer("EmojicordWebCallback/1.1"))
+				.add(new ResponseServer("Emojicord/1.1"))
 				.add(new ResponseContent())
 				.add(new ResponseConnControl())
 				.build();
@@ -75,49 +75,63 @@ public class CallbackServerInstance {
 				final HttpRequest request, final HttpResponse response,
 				final HttpContext context
 		) throws HttpException, IOException {
-			response.addHeader(new BasicHeader("Access-Control-Allow-Origin", "*"));
-			response.addHeader(new BasicHeader("Access-Control-Allow-Credentials", "true"));
+			response.addHeader(new BasicHeader("Access-Control-Allow-Origin", "https://emojicord.teamfruit.net"));
 			response.addHeader(new BasicHeader("Access-Control-Allow-Methods", "POST, OPTIONS"));
-			response.addHeader(new BasicHeader("Access-Control-Allow-Headers", "Content-Type, Accept, X-Requested-With, remember-me"));
+			response.addHeader(new BasicHeader("Access-Control-Allow-Headers", "Content-Type"));
+			response.addHeader(new BasicHeader("Access-Control-Max-Age", "86400"));
 			response.addHeader(new BasicHeader(HttpHeaders.ACCEPT, "application/json"));
 			response.addHeader(new BasicHeader(HttpHeaders.ACCEPT_CHARSET, "utf-8"));
 
-			if (request.getRequestLine().getMethod().toUpperCase(Locale.ENGLISH).equals("OPTIONS")) {
-				response.setStatusCode(HttpStatus.SC_OK);
-				response.setEntity(new StringEntity("OK", ContentType.create("text/plain", Charsets.UTF_8)));
+			final String method = request.getRequestLine().getMethod().toUpperCase(Locale.ENGLISH);
+			if (method.equals("OPTIONS")) {
+				response.setStatusCode(HttpStatus.SC_NO_CONTENT);
 				Log.log.info("OPTIONS");
 				return;
 			}
 
 			final String target = request.getRequestLine().getUri();
-			Log.log.info(String.format("URL: %s, Method: %s", target, request.getRequestLine().getMethod()));
-			if (target.equals("")||target.equals("/")) {
-				final String method = request.getRequestLine().getMethod().toUpperCase(Locale.ENGLISH);
-				if (method.equals("POST")&&request instanceof HttpEntityEnclosingRequest) {
-					final byte[] data = EntityUtils.toByteArray(((HttpEntityEnclosingRequest) request).getEntity());
-					Log.log.info(StringUtils.toEncodedString(data, Charsets.UTF_8));
-					final WebCallbackModel callback = DataUtils.loadStream(
-							new ByteArrayInputStream(data),
-							WebCallbackModel.class, "Parsing Web Callback");
-					if (callback==null) {
-						response.setStatusCode(HttpStatus.SC_BAD_REQUEST);
-						response.setEntity(new StringEntity("NG\nInvalid Json", ContentType.create("text/plain", Charsets.UTF_8)));
-						Log.log.warn("Invalid Request: Invalid Json");
-					} else {
-						response.setStatusCode(HttpStatus.SC_OK);
-						response.setEntity(new StringEntity("OK", ContentType.create("text/plain", Charsets.UTF_8)));
-						Log.log.info(String.format("key: %s, token: %s", callback.key, callback.token));
-					}
-				} else {
-					response.setStatusCode(HttpStatus.SC_BAD_REQUEST);
-					response.setEntity(new StringEntity("NG\nNo Request Data", ContentType.create("text/plain", Charsets.UTF_8)));
-					Log.log.warn("Invalid Request: No Request Data");
-				}
-			} else {
+			Log.log.info(String.format("URL: %s, Method: %s", target, method));
+
+			if (!target.equals("/")) {
 				response.setStatusCode(HttpStatus.SC_NOT_FOUND);
-				response.setEntity(new StringEntity("NG\nNot Found", ContentType.create("text/plain", Charsets.UTF_8)));
+				response.setEntity(new StringEntity("NG\nNot Found", ContentType.create("text/plain", StandardCharsets.UTF_8)));
 				Log.log.warn("Invalid Request: Not Found");
+				return;
 			}
+
+			if (!method.equals("POST")) {
+				response.setStatusCode(HttpStatus.SC_METHOD_NOT_ALLOWED);
+				response.setEntity(new StringEntity("NG\nMethod Not Allowed", ContentType.create("text/plain", StandardCharsets.UTF_8)));
+				Log.log.warn("Invalid Request: Method Not Allowed");
+				return;
+			}
+
+			if (!(request instanceof HttpEntityEnclosingRequest)) {
+				response.setStatusCode(HttpStatus.SC_BAD_REQUEST);
+				response.setEntity(new StringEntity("NG\nNo Request Data", ContentType.create("text/plain", StandardCharsets.UTF_8)));
+				Log.log.warn("Invalid Request: No Request Data");
+				return;
+			}
+
+			final HttpEntityEnclosingRequest eRequest = (HttpEntityEnclosingRequest) request;
+			final HttpEntity entity = eRequest.getEntity();
+			//if (eRequest.expectContinue()) {}
+			final byte[] data = EntityUtils.toByteArray(entity);
+			Log.log.info(StringUtils.toEncodedString(data, StandardCharsets.UTF_8));
+			final WebCallbackModel callback = DataUtils.loadStream(
+					new ByteArrayInputStream(data),
+					WebCallbackModel.class, "Parsing Web Callback");
+
+			if (callback==null||StringUtils.isEmpty(callback.key)||StringUtils.isEmpty(callback.token)) {
+				response.setStatusCode(HttpStatus.SC_BAD_REQUEST);
+				response.setEntity(new StringEntity("NG\nInvalid Json", ContentType.create("text/plain", StandardCharsets.UTF_8)));
+				Log.log.warn("Invalid Request: Invalid Json");
+				return;
+			}
+
+			response.setStatusCode(HttpStatus.SC_OK);
+			response.setEntity(new StringEntity("OK", ContentType.create("text/plain", StandardCharsets.UTF_8)));
+			Log.log.info(String.format("key: %s, token: %s", callback.key, callback.token));
 		}
 	}
 
