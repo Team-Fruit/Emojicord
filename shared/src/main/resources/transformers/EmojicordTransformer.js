d11c3a76ce6bcbcf1ab35dbcfc7fcf1f4e7a2704
Kamesuta <kamesuta@gmail.com>
2020-05-24 00:51:08 +0900

    coremod

--- shared/src/main/resources/transformers/EmojicordTransformer.js
+++ shared/src/main/resources/transformers/EmojicordTransformer.js
@@ -1,20 +1,25 @@
 var ASMAPI = Java.type('net.minecraftforge.coremod.api.ASMAPI');
 
 function initializeCoreMod() {
+    var transformers = {};
     ASMAPI.log("INFO", "EmojicordTransformer initialized");
-    var FontRendererTransform = this.engine.factory.scriptEngine.compile('Java.type(\"net.teamfruit.emojicord.asm.FontRendererTransform\");').eval();
-    return {
-        'FontRendererTransform': {
+    var EmojicordTransformer = this.engine.factory.scriptEngine.compile('Java.type(\"net.teamfruit.emojicord.asm.EmojicordTransformer\");').eval();
+    var emojicordTransformer = new EmojicordTransformer();
+    Array.prototype.forEach.call(emojicordTransformer.transformers,function(transform) {
+        var simpleName = EmojicordTransformer.getSimpleClassName(transform);
+        var className = transform.getClassName().getName();
+        transformers[simpleName] = {
             'target': {
                 'type': 'CLASS',
-                'name': 'net.minecraft.client.gui.FontRenderer'
+                'name': className
             },
             'transformer': function (node) {
-                ASMAPI.log("INFO", "EmojicordTransformer.FontRendererTransform start patching");
-                new FontRendererTransform().apply(node)
-                ASMAPI.log("INFO", "EmojicordTransformer.FontRendererTransform patched");
+                ASMAPI.log("INFO", "Patching" + className + " (class: " + node.name + ")");
+                transform.apply(node)
+                ASMAPI.log("INFO", "Finished Patching " + className + " (class: " + node.name + ")");
                 return node;
             }
-        }
-    };
+        };
+    });
+    return transformers;
 }
